f6b488ce43a8e44e9373e3465b29860873dbe3ae
Inyang Andino <doninyangm@gmail.com>
2020-01-25 06:50:13 +0100

    add item to list via js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,36 @@ list.addEventListener('click', function(e){
     }
 });
 
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+
+    //create elements
+    const item = document.createElement('div');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('button');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    //style the elemenst
+    item.classList.add('item'); // style.class = "item";
+    bookName.classList.add('title'); //.style.class = "title";
+    deleteBtn.classList.add('delete'); //style.class = "delete";
+
+    //append to document
+    item.appendChild(bookName);
+    item.appendChild(deleteBtn);
+    list.appendChild(item);
+
+    //clear values
+    addForm.querySelector('input[type="text"]').value = "";
+});
+
 // const btn = document.querySelectorAll(".delete");
 
 // btn.forEach(function(btn){
